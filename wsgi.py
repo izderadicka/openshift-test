af51707caa21545ed151524e2e7b14e906b05dc6
izderadicka <ivan.zderadicka@gmail.com>
2015-06-27 10:41:09 +0200

    flask

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,7 @@
 #!/usr/bin/env python
 import os
 
-def application(environ, start_response):
-
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        response_body = '''<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Test page</title>
-
-</head>
-<h1>Test page</h1>
-</body>
-</html>'''
-
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body.encode('utf-8') ]
-
+from flaskapp import app as application
 #
 # Below for testing only
 #
